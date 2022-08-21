aac4bd11a0aa2698a38be44257a4b90b42c46fa2
m_junaid_07 <junaid@xenex-media.com.au>
2022-08-21 10:48:27 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { BackendService } from './backend.service';
 
 @Component({
@@ -6,40 +13,138 @@ import { BackendService } from './backend.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  selectBtn!: ElementRef<HTMLElement>;
+  @ViewChild('optionButton', {
+    read: ElementRef,
+    static: false,
+  })
+  set opBtn(elRef: ElementRef<HTMLElement>) {
+    if (elRef) {
+      this.selectBtn = elRef;
+    }
+  }
+
+  ngAfterViewInit() {}
+
   questions: any[] = [
-    'Kuala Lumpur is the capital of',
+    ' is the capital of',
     'Which country does this flag belong to?  ',
   ];
-  currCountry: any;
+  endQuiz: boolean = false;
+  score: number = 0;
+  currCountry: any = [];
   currQuestion: any[] = [];
-  countriesAsked: number[] = [];
+  correctOption: any;
+  userOption: number = -1;
+  countriesAsked: any[] = []; //used to keep track of countries that have been already asked
   countries: any;
-  constructor(private countryData: BackendService) {
+  isFlagQuestion: boolean = false;
+  isCaptialQuestion: boolean = false;
+  fourRandomCountries: any[] = [];
+  constructor(
+    private countryData: BackendService,
+    private spinner: NgxSpinnerService,
+    private elem: ElementRef
+  ) {
     this.countryData.getCountries().subscribe((data) => {
+      this.spinner.hide();
       this.countries = data;
-      // this.setRandomCountry();
-      this.setRandomQuestion();
+      this.startTheQuiz();
     });
   }
+  startTheQuiz() {
+    this.endQuiz = false;
+    this.setRandomQuestion();
+    this.fourRandomCountries = [];
+    this.setFourRandomCountries(this.countries);
+  }
+
   getRandomIndex(arrLength: number) {
     return Math.floor(Math.random() * arrLength);
   }
-  setRandomCountry() {
-    let randomCountryIndex = this.getRandomIndex(this.countries.length);
-    if (this.countriesAsked.indexOf(randomCountryIndex) === -1) {
-      this.currCountry.push(this.countries[randomCountryIndex]);
+  // 01
+  setRandomQuestion() {
+    let randomQuestionIndex = this.getRandomIndex(this.questions.length);
+    this.currQuestion = this.questions[randomQuestionIndex];
+    if (this.currQuestion.includes('capital')) {
+      this.isCaptialQuestion = true;
+      this.isFlagQuestion = false;
     } else {
-      this.setRandomCountry();
+      this.isFlagQuestion = true;
+      this.isCaptialQuestion = false;
     }
   }
-  setRandomQuestion() {
-    // let randomQuestionIndex = this.getRandomIndex(this.questions.length);
-    // this.currQuestion.push(this.questions[randomQuestionIndex]);
-    console.log(this.countries);
-    
-    
+  // 02
+  setFourRandomCountries(arr: any[]) {
+    for (let i = 0; i < 4; i++) {
+      this.fourRandomCountries.push(arr[i]);
+    }
+    this.correctOption = this.getRandomIndex(this.fourRandomCountries.length);
+    this.currCountry = this.fourRandomCountries[this.correctOption];
+  }
+  selectedOption(no: number) {
+    this.elem.nativeElement.querySelector('.please-select').style.display =
+      'none';
+    this.userOption = no;
+  }
+  tryAgain() {
+    this.score = 0;
+    this.startTheQuiz();
+  }
+  toggleButtonColor(selected?: number, btn?: any) {
+    btn.classList.add('selected');
+
+    document.querySelectorAll('.option').forEach((e, i) => {
+      if (i != selected) {
+        e.classList.remove('selected');
+      }
+    });
+  }
+  resetOptions() {
+    this.elem.nativeElement
+      .querySelectorAll('.option')
+      .forEach((e: any, i: any) => {
+        if (e.classList.contains('is-right')) {
+          e.classList.remove('is-right');
+        }
+      });
+  }
+  checkAnswer(msg: any) {
+    if (this.userOption != -1) {
+      msg.style.display = 'none';
+      if (this.userOption == this.correctOption) {
+        this.score++;
+        document.querySelectorAll('.option').forEach((e, i) => {
+          if (e.classList.contains('selected')) {
+            e.classList.remove('selected');
+            e.classList.add('is-right');
+          }
+        });
+        setTimeout(() => {
+          this.resetOptions();
+          this.startTheQuiz();
+        }, 500);
+      } else {
+        document.querySelectorAll('.option').forEach((e, i) => {
+          if (this.userOption == i) {
+            if (e.classList.contains('selected')) {
+              e.classList.remove('selected');
+              e.classList.add('is-wrong');
+            }
+          }
+        });
+        setTimeout(() => {
+          this.resetOptions();
+          this.endQuiz = true;
+        }, 500);
+      }
+    } else {
+      msg.style.display = 'block';
+    }
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.spinner.show();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent],
 })
